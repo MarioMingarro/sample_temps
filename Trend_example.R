edaa911618239780555676d86ddb7f06b0294089
Mario Mingarro <60926549+MarioMingarro@users.noreply.github.com>
2022-07-01 11:31:13 +0200

    new

--- Trend_example.R
+++ Trend_example.R
@@ -74,7 +74,9 @@ tabla_ind <- data.frame(
 # Bucle para calcular las tendencias de cada una de las especies
 
 for (n in 1:length(spp)) {                             # Bucle para actuar sobre cada una de las especies
-  ind <- filter(Data, Especie == spp[n])              # Filtra la especie 
+  ind <- Data %>% 
+    filter( Especie == spp[n]) %>%
+    mutate(group = "i")              # Filtra la especie 
   
   if (nrow(ind) > 10) {                               # Condicional "SI" para seleccionar aquellas especies con mas de 10 registros
     for (i in 1:5) {                                  # bucle para cada una de las variables independientes
@@ -88,7 +90,10 @@ for (n in 1:length(spp)) {                             # Bucle para actuar sobre
           "95_max" = NA,
           "95_min" = NA,
           "F" = NA,
-          "Dif" = NA
+          "Dif_1_pvalue" = NA,
+          "Dif_2_coef" = NA,
+          "Dif_2_pvalue" = NA,
+          "Dif_2_F" = NA
         )
         # General
         model_g = lm(formula(paste(y[i], paste(  # Crea de nuevo el modelo general para la posterior comparación
@@ -97,10 +102,11 @@ for (n in 1:length(spp)) {                             # Bucle para actuar sobre
         
         tabla$Spp <- unique(ind[[1]])
         tabla$Variable <- y[i]
-        model_i <-
+        model_i <-                             # Crea el modelo de cada especie para la posterior comparación
           lm(formula(paste(y[i], paste(
             x, collapse = "+"
           ), sep = " ~ ")), data = ind)
+        
         tabla$Trend <- model_i$coefficients[[2]]
         tabla$t <- summary(model_i)$coefficients[2, 3]
         tabla$p <- summary(model_i)$coefficients[2, 4]
@@ -109,6 +115,11 @@ for (n in 1:length(spp)) {                             # Bucle para actuar sobre
         tabla$F <- summary(model_i)$fstatistic[1]
         
         
+        # Dos formas de ver si las tendencias de las especies difieren de la general
+        
+        # Método 1
+        # Obtener una estadística z al encontrar la diferencia entre los dos coeficientes 
+        # y luego dividirla por un error estándar combinado. Ver https://www.jstor.org/stable/2782277?seq=1#page_scan_tab_contents
         
         b_g <- summary(model_g)$coefficients[2,1] # Tendencia general
         se_g <- summary(model_g)$coefficients[2,2] # Desviación estándar general
@@ -116,8 +127,29 @@ for (n in 1:length(spp)) {                             # Bucle para actuar sobre
         se_i <- summary(model_i)$coefficients[2,2] # desciación estándar de especie del bucle
         
         
-        tabla$Dif <- 2*pnorm(-abs(compare.coeff(b_g,se_g,b_i,se_i))) # Fórmula utilizando la función "compare.coeff"
-                                                                     # para ver las diferencias entre pendientes
+        tabla$Dif_1_pvalue <- 2*pnorm(-abs(compare.coeff(b_g,se_g,b_i,se_i))) # Fórmula utilizando la función "compare.coeff"
+        
+        # Metodo 2  
+        # Crear un modelo incroporando la interación de la pendiente general con la de la especie Variable independiente*Año_Mes
+        # el coeficiente indica que la pendiente d ela especie es mayor o menor que la pendiente del conjunto de datos.
+        # Tambien observamos el pvalor para ver si es significativo (inf a 0,05 son pendientes diferentes)
+        
+        
+        gen <- Data %>%
+          mutate(group = "g")
+        
+        dat <- rbind(gen,ind)
+        
+        
+        model_int = lm(formula(paste(y[i], paste(  # Crea de nuevo el modelo general para la posterior comparación
+          x,"*df", collapse = "+"
+        ), sep = " ~ ")), data = dat)
+        
+       
+        Dif_2_coef$summary(model_int)$coefficients[4,1]
+        Dif_2_pvalue$summary(model_int)$coefficients[4,4]
+        Dif_2_F$summary(model_int)$fstatistic[1]
+       
         tabla_ind <- rbind(tabla_ind, tabla)  # Unimos tablas
         
       }, error = function(e) {                        # Si la función tryChach da error ejecuta esta parte del codigo
@@ -134,19 +166,7 @@ for (n in 1:length(spp)) {                             # Bucle para actuar sobre
   }
 }
 
-############################################
 
-
-model_g = lm(Lat ~ Año, data = Data)
-model_i = lm(Lat ~ Año, data = subset(Data,Data$Especie == "Volinus sticticus"))
-
-b_g <- summary(model_g)$coefficients[2,1]
-se_g <- summary(model_g)$coefficients[2,2]
-b_i <- summary(model_i)$coefficients[2,1]
-se_i <- summary(model_i)$coefficients[2,2]
-
-p_value = 2*pnorm(-abs(compare.coeff(b_g,se_g,b_i,se_i)))
-p_value #inf 0.05
 
 ############################################
 spp[45]
@@ -161,21 +181,28 @@ ggplot() +
 
 ###############################
 
-
-
-g <- Data %>% select(Lat, Año_Mes) %>%
-  mutate(df = "g")
-
-i <-  Data %>%
-  subset(Data$Especie == "Platytomus tibialis") %>%
-  select(Lat, Año_Mes) %>%
-  mutate(df = "i")
-
-dat <- rbind(g,i)
+# ind <-  Data %>%
+#   subset(Data$Especie == "Platytomus tibialis") %>%
+#   select(Lat, Año_Mes) %>%
+#   mutate(df = "i")
 
 ggplot(dat,aes(x=Año_Mes,y=Lat,col=df)) + 
-  geom_point() + 
-  geom_smooth(method="lm")
+  # geom_point() + 
+  #   geom_smooth(method="lm")
 
-summary(lm(Lat~Año_Mes*df,data=dat))
+  # model_int <- lm(Lat~Año_Mes*df,data=dat)
+  #
 
+############################################
+
+
+model_g = lm(Lat ~ Año, data = Data)
+model_i = lm(Lat ~ Año, data = subset(Data,Data$Especie == "Volinus sticticus"))
+
+b_g <- summary(model_g)$coefficients[2,1]
+se_g <- summary(model_g)$coefficients[2,2]
+b_i <- summary(model_i)$coefficients[2,1]
+se_i <- summary(model_i)$coefficients[2,2]
+
+p_value = 2*pnorm(-abs(compare.coeff(b_g,se_g,b_i,se_i)))
+p_value #inf 0.05
